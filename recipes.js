b6e129ff7072c1dd06716161175011050253ab46
Kailey Wong <kaileyswong@gmail.com>
2020-08-05 22:06:16 -0700

    initial commit

---
+++ recipes.js
@@ -0,0 +1,59 @@
+var menuToggle = document.getElementById("menu-toggle");
+var activeElements = document.querySelectorAll(".active-element");
+
+var toggledMenu = menuToggle.addEventListener("click", () => {
+    activeElements.forEach(function(e) {
+        e.classList.toggle("active");
+    });
+})
+
+function closeMenu() {
+    activeElements.forEach(function(e) {
+        e.classList.toggle("active");
+    })
+}
+
+function openRecipe(event, recipeName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(recipeName).style.display = "block";
+    event.currentTarget.className += " active";
+}
+
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("slides");
+    var dots = document.getElementsByClassName("demo");
+    var caption = document.getElementById("caption");
+    if (n > slides.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = slides.length }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    caption.innerHTML = dots[slideIndex - 1].alt;
+}
