28c1a2062f6ffa2bdc9f2112f8f0c56387f264b7
Kailey Wong <kaileyswong@gmail.com>
2020-08-08 15:03:02 -0700

    Finished image slideshow, added responsive nav

--- recipes.js
+++ recipes.js
@@ -1,18 +1,3 @@
-var menuToggle = document.getElementById("menu-toggle");
-var activeElements = document.querySelectorAll(".active-element");
-
-var toggledMenu = menuToggle.addEventListener("click", () => {
-    activeElements.forEach(function(e) {
-        e.classList.toggle("active");
-    });
-})
-
-function closeMenu() {
-    activeElements.forEach(function(e) {
-        e.classList.toggle("active");
-    })
-}
-
 function openRecipe(event, recipeName) {
     var i, tabcontent, tablinks;
     tabcontent = document.getElementsByClassName("tabcontent");
@@ -31,12 +16,10 @@ function openRecipe(event, recipeName) {
 var slideIndex = 1;
 showSlides(slideIndex);
 
-// Next/previous controls
 function plusSlides(n) {
     showSlides(slideIndex += n);
 }
 
-// Thumbnail image controls
 function currentSlide(n) {
     showSlides(slideIndex = n);
 }
@@ -46,14 +29,30 @@ function showSlides(n) {
     var slides = document.getElementsByClassName("slides");
     var dots = document.getElementsByClassName("demo");
     var caption = document.getElementById("caption");
-    if (n > slides.length) { slideIndex = 1 }
-    if (n < 1) { slideIndex = slides.length }
+    if (n > slides.length) {
+        slideIndex = 1;
+    }
+    if (n < 1) {
+        slideIndex = slides.length;
+    }
     for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
     }
     for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
+        dots[i].className = dots[i].className.replace("active", "");
     }
     slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
     caption.innerHTML = dots[slideIndex - 1].alt;
+}
+
+function responsiveNav() {
+    var nav = document.getElementsByClassName("navbar")[0];
+    if (nav.className === "navbar") {
+        nav.className += " responsive";
+    } else {
+        nav.className = "navbar";
+    }
+    alert("active")
+
 }
